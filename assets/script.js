4f8632b346cc7817f6aaac3db29a350882b2463b
Peter Yockey <peterjyockey@gmail.com>
2023-04-01 15:26:51 -0500

    fixed file location problems

--- assets/script.js
+++ assets/script.js
@@ -6,7 +6,6 @@ var numbers = [ "1", "2", "3", "4", "5", "6", "7", "8", "9", "0" ];
 var specialCharacters = [ "!", "$", "%", "&", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~", ];
 var numOfCharacters 
 
-
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
